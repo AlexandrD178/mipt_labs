c230796fdf50d2e32a0eecac785f1aeb78b169f7
alexd <dirun.aa@phystech.edu>
2025-03-13 14:09:52 +0300

    Test

--- output/First_Lab_Di.cpp
+++ output/First_Lab_Di.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <chrono>
 
-int rand_key(int len) {
+int rand_key(int len) {                     //function returning one random value from (0, len)
     unsigned seed = 1001;
     std::default_random_engine rng(seed);
     std::uniform_int_distribution<unsigned> dstr(0, len - 1);
@@ -11,7 +11,7 @@ int rand_key(int len) {
 
 }
 
-bool searching_func(int len, int key, int arr[]) {
+bool searching_func(int len, int key, int arr[]) {              //function, which executive time I want to measure
     for (int counter = 0; counter < len; ++counter) {
         if (arr[counter] == key) return 1;
     }
@@ -19,15 +19,15 @@ bool searching_func(int len, int key, int arr[]) {
     return 0;
 }
 
-float counting_time(int len) {
-    int arr[len] = {};
+float counting_time(int len) {              
+    int arr[len] = {};                  //creat ordered array {1, 2, ... len}
     for (int i = 0; i < len; ++i) 
         arr[i] = i;
 
-    int key = rand_key(len);
+    int key = len+10;           //random value, which I want to search
         
 
-    auto begin = std::chrono::steady_clock::now();
+    auto begin = std::chrono::steady_clock::now();         
     for (unsigned cnt = 100000; cnt !=0; --cnt) {
         searching_func(len, key, arr);
     }
@@ -40,7 +40,7 @@ float counting_time(int len) {
 
 
 int main() {
-    for (int len = 100; len < 1000000; len += 10000) {
+    for (int len = 100; len < 1000000; len += 20000) {
         std::cout << len << ' ' << counting_time(len) << '\n';
     }
     std::cout << std::endl;
